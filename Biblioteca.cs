21cc33fcfc476e1098e647163b9d0517bf75fc94
EDY150423 <edgarisai.ramirezcontreras.gen23@cbtis105.edu.mx>
2025-03-24 10:17:43 -0600

    Dia 1

---
+++ Biblioteca.cs
@@ -0,0 +1,9 @@
+public class Biblioteca
+{
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public int Añodepublicacion { get; set; }
+    public decimal Precio { get; set; }
+    public bool Disponible { get; set; }
+    public string Categoria {get}
+}
